63e35413d7eef7dec6c13a139a4ba3c4107cc866
Celia <cefloresa@alumnos.unex.es>
2018-12-11 16:09:41 +0100

    2

---
+++ Practica2-Robotica/choca/src/CMakeFiles/choca.dir/cmake_clean.cmake
@@ -0,0 +1,34 @@
+file(REMOVE_RECURSE
+  "choca_autogen"
+  "CMakeFiles/choca_autogen.dir/AutogenOldSettings.cmake"
+  "CommonBehavior.cpp"
+  "CommonBehavior.h"
+  "DifferentialRobot.cpp"
+  "DifferentialRobot.h"
+  "GenericBase.cpp"
+  "GenericBase.h"
+  "Laser.cpp"
+  "Laser.h"
+  "ui_mainUI.h"
+  "CMakeFiles/choca.dir/specificworker.cpp.o"
+  "CMakeFiles/choca.dir/specificmonitor.cpp.o"
+  "CMakeFiles/choca.dir/home/celia/robocomp/classes/rapplication/rapplication.cpp.o"
+  "CMakeFiles/choca.dir/home/celia/robocomp/classes/sigwatch/sigwatch.cpp.o"
+  "CMakeFiles/choca.dir/home/celia/robocomp/classes/qlog/qlog.cpp.o"
+  "CMakeFiles/choca.dir/main.cpp.o"
+  "CMakeFiles/choca.dir/genericmonitor.cpp.o"
+  "CMakeFiles/choca.dir/commonbehaviorI.cpp.o"
+  "CMakeFiles/choca.dir/genericworker.cpp.o"
+  "CMakeFiles/choca.dir/CommonBehavior.cpp.o"
+  "CMakeFiles/choca.dir/DifferentialRobot.cpp.o"
+  "CMakeFiles/choca.dir/GenericBase.cpp.o"
+  "CMakeFiles/choca.dir/Laser.cpp.o"
+  "CMakeFiles/choca.dir/choca_autogen/mocs_compilation.cpp.o"
+  "../bin/choca.pdb"
+  "../bin/choca"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/choca.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
